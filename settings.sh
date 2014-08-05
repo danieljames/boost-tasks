9a44a8747350edd260f05d30049a0e0f795d1ec3
Daniel James <daniel@calamity.org.uk>
2014-08-05 21:12:00 +0100

    Move settings into separate file.

---
+++ settings.sh
@@ -0,0 +1,25 @@
+###############################################################################
+#
+# Configuration
+
+# Libraries to build documentation for
+export STANDALONE_DOCUMENTATION="geometry phoenix fusion spirit spirit/repository crc algorithm context coroutine numeric/odeint log tti functional/factory functional/forward range utility core convert"
+
+# Libraries to upload documentation for. Uploads all of libs/name, so no need
+# to include nested libraries.
+export UPLOAD_DOCUMENTATION="log geometry phoenix fusion spirit crc algorithm context coroutine numeric/odeint container tti functional/factory functional/forward range utility core convert"
+
+# Compiler details, leave ccache blank if you don't use it.
+export CCACHE_BIN="ccache"
+export CXX_BIN="g++"
+export CXX_FLAGS="--std=c++0x"
+export B2_TOOLSET="gcc"
+
+# TODO: Setting DOXYGEN_BIN to anything else won't work for geometry.
+# TODO: Perhaps write a shell script to the bin directory.
+export DOXYGEN_BIN="doxygen"
+export RAPIDXML_VERSION=1.13
+export SOURCEFORGE_USERNAME=danieljames
+
+export RAPIDXML_FILENAME=rapidxml-${RAPIDXML_VERSION}.zip
+export GIT_URL=$root/../boost-update/repos/mirror/
