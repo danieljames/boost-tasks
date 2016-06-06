44d7b7a3ba2a561f0c520bc85d3a4ca5096583bc
Daniel James <daniel@calamity.org.uk>
2016-06-06 09:33:03 +0100

    New error handler.

--- src/_init.php
+++ src/_init.php
@@ -13,18 +13,40 @@ date_default_timezone_set('UTC');
 
 // Die on all errors.
 
-function myErrorHandler($errno, $errstr, $errfile, $errline)
-{
+function myErrorHandler($message) {
     if (Log::$log) {
-        Log::error("{$errfile}:{$errline}: {$errstr}");
+        Log::error($message);
+    }
+    else if (array_key_exists('SERVER_PROTOCOL', $_SERVER)) {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+        // TODO: Don't html encode message if writing text.
+        echo htmlentities($message),"\n";
+    }
+    else if (defined('STDERR')) {
+        fputs(STDERR, $message);
     }
     else {
-        fputs(STDERR, "{$errfile}:{$errline}: {$errstr}");
+        echo("{$message}\n");
     }
     exit(1);
 }
 
-set_error_handler('myErrorHandler');
+set_error_handler(function($errno, $errstr, $errfile, $errline) {
+    myErrorHandler("{$errfile}:{$errline}: {$errstr}");
+});
+
+set_exception_handler(function($e) {
+    myErrorHandler($e->getMessage());
+});
+
+register_shutdown_function(function() {
+    $last_error = error_get_last();
+    if ($last_error && $last_error['type'] & (E_ERROR|E_PARSE|E_CORE_ERROR|E_COMPILE_ERROR)) {
+        if (array_key_exists('SERVER_PROTOCOL', $_SERVER)) {
+           header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+        }
+    }
+});
 
 // Initialise global state.
 
