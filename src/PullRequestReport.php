466806c8bb61841b1762394af955c19f4d246ac7
Daniel James <daniel@calamity.org.uk>
2017-11-07 22:49:37 +0000

    Remove updating pull requests from webhooks

--- src/PullRequestReport.php
+++ src/PullRequestReport.php
@@ -13,28 +13,11 @@ use BoostTasks\Db;
 class PullRequestReport extends Object {
     static function update($all = false) {
         $report = new PullRequestReport;
-        if ($all) {
-            $report->fullUpdate();
-        }
-        else {
-            $report->updateFromQueue();
-        }
+        $report->fullUpdate();
         $report->write();
     }
 
-    public function webhookDatabase() {
-        // TODO: This shouldn't be hard-coded.
-        static $database = null;
-        if (!$database) {
-            $database = Db::create('sqlite:'.BOOST_TASKS_ROOT.'/var/webhook-data/cache.db');
-        }
-        return $database;
-    }
-
     public function fullUpdate() {
-        // Get queue position before downloading pull requests.
-        $last_event_id = $this->webhookDatabase()->getCell('SELECT max(`id`) FROM pull_request_event');
-
         // Download pull requests.
         $pull_requests = Array();
         foreach (EvilGlobals::githubCache()->iterate('/orgs/boostorg/repos') as $repo) {
@@ -85,66 +68,7 @@ class PullRequestReport extends Object {
             $record->trash();
         }
 
-        $queue = $this->loadQueue($db);
-        $queue->last_github_id = $last_event_id;
-        $queue->store();
-
         $db->commit();
-    }
-
-    public function updateFromQueue() {
-        $webhook_database = $this->webhookDatabase();
-        $db = EvilGlobals::database();
-        $db->begin();
-        $queue = $this->loadQueue($db);
-        foreach($webhook_database->getAll('SELECT * FROM pull_request_event WHERE id > ? ORDER BY id', array($queue->last_github_id)) AS $row) {
-            $record = $db->load('pull_request', $row['pull_request_id']);
-            if ($record) {
-                assert($record->repo_full_name === $row['repo_full_name']);
-                assert($record->pull_request_number === $row['pull_request_number']);
-                assert($record->pull_request_url === $row['pull_request_url']);
-                assert($record->pull_request_created_at === $row['pull_request_created_at']);
-            }
-
-            switch($row['pull_request_state']) {
-            case 'open':
-                if (!$record) {
-                    $record = $db->dispense('pull_request');
-                    $record->id = $row['pull_request_id'];
-                    $record->repo_full_name = $row['repo_full_name'];
-                    $record->pull_request_number = $row['pull_request_number'];
-                    $record->pull_request_url = $row['pull_request_url'];
-                    $record->pull_request_created_at = $row['pull_request_created_at'];
-                }
-                $record->pull_request_title = $row['pull_request_title'];
-                $record->pull_request_updated_at = $row['pull_request_updated_at'];
-                $record->store();
-                break;
-            case 'closed':
-                if ($record) {
-                    $record->trash();
-                }
-                break;
-            default:
-                Log::Error("Unknown pull request state: {$row['pull_request_state']}");
-            }
-
-            $queue->last_github_id = $row['id'];
-            $queue->store();
-        }
-        $db->commit();
-    }
-
-    public function loadQueue($db) {
-        $queue = $db->findOne('queue', 'name = ? AND type = ?',
-            array('pull_request','PullRequestWebhook'));
-        if (!$queue) {
-            $queue = $db->dispense('queue');
-            $queue->name = 'pull_request';
-            $queue->type = 'PullRequestWebhook';
-            $queue->last_github_id = 0;
-        }
-        return $queue;
     }
 
     public function write() {
