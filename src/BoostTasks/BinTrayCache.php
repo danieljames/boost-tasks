08617a48e721cf272a8760b467bad75d4af7a446
Daniel James <daniel@calamity.org.uk>
2017-11-09 20:14:36 +0000

    Remove a TODO that has already been todone

--- src/BoostTasks/BinTrayCache.php
+++ src/BoostTasks/BinTrayCache.php
@@ -276,7 +276,6 @@ class BinTrayCache_FileDetails {
 
     // Downloads file from $url to local path $dst_path
     // Throws RuntimeException on failure.
-    // TODO: Download to temporary file and move into position.
     function downloadFile($url, $dst_path) {
         $download_fh = fopen($url, 'rb', false, $this->cache->stream_context);
 
