7c5c391be6505bd99734eb28fa4e716820592d63
Daniel James <daniel@calamity.org.uk>
2019-09-02 21:27:59 +0100

    Fix starting super project from scratch

--- src/BoostTasks/SuperProject.php
+++ src/BoostTasks/SuperProject.php
@@ -78,7 +78,7 @@ class SuperProject extends Repo {
             $queue->downloadMoreEvents();
 
             foreach ($queue->getEvents($github_start_id) as $event) {
-                if ($event->branch == $this->submodule_branch) {
+                if ($event->branch == $self->submodule_branch) {
                     if (array_key_exists($event->repo, $submodules)) {
                         $payload = json_decode($event->payload);
                         assert($payload);
